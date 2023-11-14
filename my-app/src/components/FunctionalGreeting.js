e736ef41136e18142ed03979f96e4f91283bb8bb
Brionconroy <razvanbrion@gmail.com>
2023-11-14 10:04:53 +0000

    Add Functional Component

---
+++ my-app/src/components/FunctionalGreeting.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// function FunctionalGreeting() {
+//     return <h1>React here, Whos there?</h1>
+// }
+
+const FunctionalGreeting = () => <h1>React here, Whos there?!</h1>
+
+export default FunctionalGreeting;
