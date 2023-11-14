98b45e44ad5c2d5ad5ba24d65a63a15b9d8d29c9
Brionconroy <razvanbrion@gmail.com>
2023-11-14 16:06:55 +0000

    With callback fixed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,16 @@
 // import logo from './logo.svg';
 import './App.css';
 // import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
-import StatefulGreeting from './components/StatefulGreeting';
+// import StatefulGreeting from './components/StatefulGreeting';
+import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
+
 
 function App() {
   return (
     <div className="App">
       {/* <FunctionalGreetingWithProps greeting= "Well!!!!" name= "It Is I Tommey" age="102" /> */}
-      <StatefulGreeting greeting= "Im a tool!!!" name= "Me tommy"/>
+      {/* <StatefulGreeting greeting= "Im a tool!!!" name= "Me tommy"/> */}
+      <StatefulGreetingWithCallback/>
     </div>
   );
 }
